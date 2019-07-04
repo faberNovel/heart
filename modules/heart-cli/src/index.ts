73ac70fb3bedd515a92643a42bc485b74be997a2
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2019-07-04 14:13:17 +0000

    Resolve "Run an analysis by specifying a file path"

--- modules/heart-cli/src/index.ts
+++ modules/heart-cli/src/index.ts
@@ -1,45 +1,87 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 import { ModuleAnalysisInterface, ModuleLoader } from '@fabernovel/heart-core';
 import * as program from 'commander';
+import { readFileSync } from 'fs';
+import { isAbsolute } from 'path';
 
 import App from './App';
+import InputAnalysisValidation from './Validation/InputAnalysisValidation';
 
-const moduleLoader = new ModuleLoader(true);
+const moduleLoader = new ModuleLoader(false);
 
 moduleLoader.load()
   .then((modules: any[]) => {
     const app = new App(modules);
 
-    // list the commands that will be available in the CLI, according to the loaded modules.
-    // for now, the only available commands are the one related to analysis modules.
-    const commands = app.analysisModules.map((module: ModuleAnalysisInterface) => {
+    // list the allowed values of the --service option
+    const serviceValues = app.analysisModules.map((module: ModuleAnalysisInterface) => {
       const analysis = new module.Analysis();
 
       return analysis.getRoutePath();
     });
 
-    // set the program version
-    program.version('1.0.0');
+    program.version('2.0.0');
 
-    // define CLI commands
-    commands.forEach((command: string) => {
-      program
-        .command([command, '<conf>'].join(' '))
-        .action((conf: string) => app.start(command, JSON.parse(conf)));
-    });
+    program
+      .command('analysis')
+        .description('Run an analysis using a single analysis service')
+        .option('-s, --service <service>', '[Mandatory] Name of the service that will perform the analysis')
+        .option('-f, --file [file]', '[Optional] Path to the JSON configuration file')
+        .option('-i, --inline [inline]', '[Optional] Inlined JSON configuration')
+        .action((cmd: program.Command) => {
+          const errors = InputAnalysisValidation.validate(cmd.service, cmd.file, cmd.inline, serviceValues);
 
-    // Print error message and --help For invalid commands
-    program.on('command:*', () => {
-      console.error('Invalid command name.\n');
-      program.help();
-      process.exit(1);
-    });
+          if (errors.length > 0) {
+            errors.forEach(error => console.error(error));
+            process.exit(1);
+          }
 
-    program.parse(process.argv);
+          let config = {};
+          let configStr = '';
+
+          if (cmd.inline) {
+            configStr = cmd.inline;
+          } else { // file: load file from the given path
+            const path = isAbsolute(cmd.file) ? cmd.file : `${process.env.PWD}/${cmd.file}`;
+
+            try {
+              configStr = readFileSync(path, 'utf8');
+            } catch (e) {
+              console.error(e);
+              process.exit(1);
+            }
+          }
+
+          try {
+            config = JSON.parse(configStr);
+          } catch (e) {
+            console.error('Cannot parse the configuration. Please check the JSON syntax.');
+            process.exit(1);
+          }
+
+          app.start(cmd.service, config);
+        });
+
+    // // prepare cli for a future release, where the CLI will be a required dependency of every module,
+    // // and Heart API will be launched from the CLI
+    // program
+    //   .command('api')
+    //     .description('Set up the API')
+    //     .option('-h, --host [host]', 'Specify the host')
+    //     .option('-p, --port [port]', 'Specify the port', parseInt);
+
+    program
+      // print error message and --help For invalid commands
+      .on('command:*', () => {
+        console.error('Invalid command name.\n');
+        program.help();
+        process.exit(1);
+      })
+      .parse(process.argv);
   })
-  .catch((e: Error) => {
-    console.error(e.message);
+  .catch(e => {
+    console.error(e);
     process.exit(1);
   });
 
