d991de1f9a40163cbbe91d85dcbd4b90511abfb6
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2021-10-15 22:35:50 +0200

    feat: add a template module used as a codebase to new module

---
+++ modules/heart-moduletpl/src/index.ts
@@ -0,0 +1,9 @@
+import ModuleTplModule from './ModuleTplModule';
+
+export default new ModuleTplModule({
+  name: 'Heart ModuleTpl',
+  service: {
+    name: 'ModuleTpl',
+    logo: 'https://gitlab.com/fabernovel/heart/raw/master/assets/images/logos/ModuleTpl.png?v=20211015'
+  },
+});
