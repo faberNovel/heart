e891726f95c01e9d00537b6c9f75b273e6e42014
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2022-08-29 19:51:49 +0000

    Resolve "Implement the threshold mechanism for the Heart Lighthouse module"

--- modules/heart-lighthouse/src/LighthouseModule.ts
+++ modules/heart-lighthouse/src/LighthouseModule.ts
@@ -1,15 +1,19 @@
-import { Module, ModuleAnalysisInterface, ModuleInterface, Report } from '@fabernovel/heart-core'
+import { Module, ModuleAnalysisInterface, ModuleInterface, Report, ThresholdInputObject, } from '@fabernovel/heart-core'
 import { Result } from 'lighthouse'
 import { runAnalysis } from './api/Client'
 import { LighthouseConfig } from './config/Config'
 import {compute} from './scoring/compute'
 
 export class LighthouseModule extends Module implements ModuleAnalysisInterface<LighthouseConfig> {
+  private thresholds?: ThresholdInputObject
+
   constructor(module: Omit<ModuleInterface, 'id'>) {
     super(module)
   }
 
-  public async startAnalysis(conf: LighthouseConfig): Promise<Report> {
+  public async startAnalysis(conf: LighthouseConfig, thresholds?: ThresholdInputObject): Promise<Report> {
+    this.thresholds = thresholds;
+
     const results = await runAnalysis(conf)
 
     return this.handleResults(results)
@@ -22,7 +26,9 @@ export class LighthouseModule extends Module implements ModuleAnalysisInterface<
       analyzedUrl: result.requestedUrl,
       date: new Date(result.fetchTime),
       service: this.service,
-      note: score.toString()
+      note: score.toString(),
+      normalizedNote: score,
+      thresholds: this.thresholds
     })
   }
 }
