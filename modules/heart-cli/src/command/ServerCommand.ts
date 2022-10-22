bfd6569a834ba1a20f949fe89c3be152be8ad59f
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2022-10-22 20:57:27 +0200

    feat: add a spinner with hearts symbols during the analysis
    refactor: improve import
    
    refactor: improve comprehension and consistency

--- modules/heart-cli/src/command/ServerCommand.ts
+++ modules/heart-cli/src/command/ServerCommand.ts
@@ -19,40 +19,33 @@ const PORT_DEFAULT = "3000"
 const PORT_REGEX =
   /^(0|[1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$/
 
-export class ServerCommand {
-  /**
-   * Create a command dedicated to the given server module
-   */
-  public static create(
-    program: Command,
-    module: ModuleServerInterface,
-    callback: (port: number, corsOptions?: CorsOptions) => void
-  ): void {
-    program
-      .command(module.id)
-      .description(`Starts the ${module.name} server`)
-      .option(
-        `-${OPTIONS.port[0]}, --${OPTIONS.port} [${OPTIONS.port}]`,
-        "Port that the server is listening to",
-        (value: string) => (PORT_REGEX.test(value) ? value : PORT_DEFAULT),
-        PORT_DEFAULT
-      )
-      .option(
-        `-${OPTIONS.cors[0]}, --${OPTIONS.cors} [${OPTIONS.cors}]`,
-        "CORS configuration, as defined in https://www.npmjs.com/package/cors#configuration-options",
-        (value: string) => {
-          try {
-            return JSON.parse(value) as CorsOptions
-          } catch (error) {
-            console.error(error)
-            program.help()
-          }
-        }
-      )
-      .action((options: Options) => {
-        const { cors, port } = options
+/**
+ * Create a command dedicated to the given server module
+ */
+export const createServerCommand = (
+  module: ModuleServerInterface,
+  callback: (port: number, corsOptions?: CorsOptions) => void
+): Command => {
+  const command = new Command(module.id)
 
-        callback(Number(port), cors)
-      })
-  }
+  command
+    .description(`Starts the ${module.name} server`)
+    .option(
+      `-${OPTIONS.port[0]}, --${OPTIONS.port} [${OPTIONS.port}]`,
+      "Port that the server is listening to",
+      (value: string) => (PORT_REGEX.test(value) ? value : PORT_DEFAULT),
+      PORT_DEFAULT
+    )
+    .option(
+      `-${OPTIONS.cors[0]}, --${OPTIONS.cors} [${OPTIONS.cors}]`,
+      "CORS configuration, as defined in https://www.npmjs.com/package/cors#configuration-options",
+      (value: string) => JSON.parse(value) as CorsOptions
+    )
+    .action((options: Options) => {
+      const { cors, port } = options
+
+      callback(Number(port), cors)
+    })
+
+  return command
 }
--- modules/heart-cli/src/index.ts
+++ modules/heart-cli/src/index.ts
@@ -1,10 +1,8 @@
-#!/usr/bin/env node
-
 import { Config, isModuleAnalysis, isModuleServer, ModuleInterface } from "@fabernovel/heart-core"
 import { Command } from "commander"
-import * as dotenv from "dotenv"
-import { AnalysisCommand } from "./command/AnalysisCommand"
-import { ServerCommand } from "./command/ServerCommand"
+import { config } from "dotenv"
+import { createAnalysisCommand } from "./command/AnalysisCommand"
+import { createServerCommand } from "./command/ServerCommand"
 import { ModuleLoader } from "./module/ModuleLoader"
 import { App } from "./App"
 import { CorsOptions } from "cors"
@@ -12,14 +10,13 @@ import { CorsOptions } from "cors"
 // set environment variables from a.env file
 // assume that the root path if the one from where the script has been called
 // /!\ this approach does not follow symlink
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-dotenv.config({ path: `${process.cwd()}/.env` })
+config({ path: `${process.cwd()}/.env` })
 
-const moduleLoader = new ModuleLoader(false)
+void (async () => {
+  const moduleLoader = new ModuleLoader(false)
 
-moduleLoader
-  .load()
-  .then((modules: ModuleInterface[]) => {
+  try {
+    const modules = await moduleLoader.load()
     const app = new App(modules)
 
     const program = new Command()
@@ -28,25 +25,28 @@ moduleLoader
     // create a command for each module
     modules.forEach((module: ModuleInterface) => {
       if (isModuleAnalysis(module)) {
-        AnalysisCommand.create(program, module, <T extends Config>(conf: T, threshold?: number) =>
+        const callback = <T extends Config>(conf: T, threshold?: number) =>
           app.startAnalysis(module, conf, threshold)
-        )
+
+        const analysisCommand = createAnalysisCommand(module, callback)
+
+        program.addCommand(analysisCommand)
       } else if (isModuleServer(module)) {
-        ServerCommand.create(program, module, (port: number, cors?: CorsOptions) =>
-          app.startServer(module, modules, port, cors)
-        )
+        const callback = (port: number, cors?: CorsOptions) => app.startServer(module, modules, port, cors)
+
+        const serverCommand = createServerCommand(module, callback)
+
+        program.addCommand(serverCommand)
       }
     })
 
-    program
-      // print error message and --help For invalid commands
+    await program
       .on("command:*", () => {
-        console.error("Invalid command name.\n")
-        program.help()
+        program.error("Invalid command name.")
       })
-      .parse(process.argv)
-  })
-  .catch((error) => {
+      .parseAsync(process.argv)
+  } catch (error) {
     console.error(error)
     process.exit(1)
-  })
+  }
+})()
