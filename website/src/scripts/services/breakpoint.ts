05c4b639f0451d43389aa69ff014d0823e61ca04
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2022-11-14 15:07:42 +0100

    chore: move the website to its own repository (#3)
    * ci: remove website-related ci scripts
    
    * feat: remove the website directory
    
    * feat: remove the Rush website-start custom command
    
    * feat: remove website from Rush managed packages
    
    * feat: update lock file

--- website/src/scripts/services/breakpoint.ts
+++
@@ -1,5 +0,0 @@
-import { Breakpoint } from "../models/breakpoint"
-
-export const BreakpointService = {
-  getFromWindow: () => (window.innerWidth > Breakpoint.MOBILE ? Breakpoint.DESKTOP : Breakpoint.MOBILE),
-}
