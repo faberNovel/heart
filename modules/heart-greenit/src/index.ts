bba64ec7adfe45ac3fd8891901bdaa5ec606229a
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2022-11-26 14:55:45 +0100

    fix: argument

--- modules/heart-greenit/src/index.ts
+++ modules/heart-greenit/src/index.ts
@@ -4,6 +4,6 @@ export default new GreenITModule({
   name: "Heart GreenIT",
   service: {
     name: "GreenIT Analysis",
-    logo: "https://raw.githubusercontent.com/faberNovel/heart/master/assets/images/logos/ecoIndex.png?v=20221126g",
+    logo: "https://raw.githubusercontent.com/faberNovel/heart/master/assets/images/logos/ecoIndex.png?v=20221126",
   },
 })
