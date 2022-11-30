0a8bb655c22165a5070a66a38ee61e2d5a949959
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2022-11-30 16:03:51 +0100

    fix: typing for the GreenIT module

--- modules/heart-greenit/src/GreenITModule.ts
+++ modules/heart-greenit/src/GreenITModule.ts
@@ -4,14 +4,14 @@ import { runAnalysis } from "./api/Client"
 import { Result } from "./api/model/Result"
 import { GreenITConfig } from "./config/Config"
 
-export class GreenITModule extends Module implements ModuleAnalysisInterface<GreenITConfig> {
+export class GreenITModule extends Module implements ModuleAnalysisInterface<GreenITConfig, Result> {
   private threshold?: number
 
   constructor(module: Omit<ModuleInterface, "id">) {
     super(module)
   }
 
-  public async startAnalysis(conf: GreenITConfig, threshold?: number): Promise<Report> {
+  public async startAnalysis(conf: GreenITConfig, threshold?: number): Promise<Report<Result>> {
     this.threshold = threshold
 
     const result = await runAnalysis(conf)
@@ -19,10 +19,11 @@ export class GreenITModule extends Module implements ModuleAnalysisInterface<Gre
     return this.handleResults(result)
   }
 
-  private handleResults(results: Result): Report {
+  private handleResults(results: Result): Report<Result> {
     return new Report({
       analyzedUrl: results.url,
       date: new Date(),
+      rawResults: results,
       note: results.ecoIndex.toString(),
       service: this.service,
       threshold: this.threshold,
