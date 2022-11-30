cce8ee261700179f333e4f52bf1d83d2634396a3
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2022-11-30 17:58:06 +0100

    fix: typings for SslLabsServer module

--- modules/heart-ssllabs-server/src/SsllabsServerModule.ts
+++ modules/heart-ssllabs-server/src/SsllabsServerModule.ts
@@ -1,10 +1,13 @@
 import { Helper, Module, ModuleAnalysisInterface, ModuleInterface, Report } from "@fabernovel/heart-core"
-import { Status } from "./api/enum/Status"
-import { Host } from "./api/model/Host"
 import { Client } from "./api/Client"
+import { Status } from "./api/enum/Status"
+import { getAveragePercentage, SsllabsServerHost } from "./api/model/Host"
 import { SsllabsServerConfig } from "./config/Config"
 
-export class SsllabsServerModule extends Module implements ModuleAnalysisInterface<SsllabsServerConfig> {
+export class SsllabsServerModule
+  extends Module
+  implements ModuleAnalysisInterface<SsllabsServerConfig, SsllabsServerHost>
+{
   private static readonly MAX_TRIES = 100
   private static readonly TIME_BETWEEN_TRIES = 10000 // 10 seconds
   private apiClient: Client
@@ -16,14 +19,17 @@ export class SsllabsServerModule extends Module implements ModuleAnalysisInterfa
     this.apiClient = new Client()
   }
 
-  public async startAnalysis(conf: SsllabsServerConfig, threshold?: number): Promise<Report> {
+  public async startAnalysis(
+    conf: SsllabsServerConfig,
+    threshold?: number
+  ): Promise<Report<SsllabsServerHost>> {
     this.threshold = threshold
     await this.apiClient.launchAnalysis(conf)
 
     return this.requestReport()
   }
 
-  private async requestReport(triesQty = 1): Promise<Report> {
+  private async requestReport(triesQty = 1): Promise<Report<SsllabsServerHost>> {
     if (triesQty > SsllabsServerModule.MAX_TRIES) {
       throw new Error(
         `The maximum number of tries (${SsllabsServerModule.MAX_TRIES}) to retrieve the report has been reached.`
@@ -35,7 +41,10 @@ export class SsllabsServerModule extends Module implements ModuleAnalysisInterfa
     return this.handleRequestScan(host, triesQty)
   }
 
-  private async handleRequestScan(host: Host, triesQty: number): Promise<Report> {
+  private async handleRequestScan(
+    host: SsllabsServerHost,
+    triesQty: number
+  ): Promise<Report<SsllabsServerHost>> {
     switch (host.status) {
       case Status.ERROR:
         throw new Error(`${host.status}: ${host.statusMessage}`)
@@ -45,16 +54,20 @@ export class SsllabsServerModule extends Module implements ModuleAnalysisInterfa
         await Helper.timeout(SsllabsServerModule.TIME_BETWEEN_TRIES)
         return this.requestReport(++triesQty)
 
-      case Status.READY:
+      case Status.READY: {
+        const note = getAveragePercentage(host.endpoints)
+
         return new Report({
           analyzedUrl: this.apiClient.getProjectUrl(),
-          note: host.getAveragePercentage().toString(),
-          normalizedNote: host.getAveragePercentage(),
-          resultUrl: this.apiClient.getAnalyzeUrl(),
           date: new Date(host.startTime),
+          rawResults: host,
+          note: note.toString(),
+          normalizedNote: note,
+          resultUrl: this.apiClient.getAnalyzeUrl(),
           service: this.service,
           threshold: this.threshold,
         })
+      }
 
       default:
         throw new Error(host.statusMessage)
