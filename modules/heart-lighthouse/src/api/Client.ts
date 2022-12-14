ed02a3383f4c7b8ad5ac18fb40d25f32a6594776
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2022-12-14 15:38:18 +0100

    fix: Chrome could not respond in CI

--- modules/heart-lighthouse/src/api/Client.ts
+++ modules/heart-lighthouse/src/api/Client.ts
@@ -3,7 +3,16 @@ import lighthouse from "lighthouse"
 import { LighthouseConfig } from "../config/Config"
 
 export async function runAnalysis(conf: LighthouseConfig): Promise<LH.Result> {
-  const chrome = await launch({ chromeFlags: ["--headless", "--no-sandbox"] })
+  const chrome = await launch({
+    chromeFlags: [
+      "--headless",
+      "--no-sandbox",
+      // The /dev/shm partition is too small in certain VM environments, causing Chrome to fail or crash (see http://crbug.com/715363).
+      // Use this flag to work-around this issue (a temporary directory will always be used to create anonymous shared memory files).
+      // @see {@link https://peter.sh/experiments/chromium-command-line-switches/#disable-dev-shm-usage}
+      "--disable-dev-shm-usage",
+    ],
+  })
 
   const runnerResult = await lighthouse(conf.url, { port: chrome.port, output: "json" }, conf.config)
   if (undefined === runnerResult) {
