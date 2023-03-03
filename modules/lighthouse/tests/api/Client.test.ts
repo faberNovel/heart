44951d08f041a6f182b3746ae9c35db520f5bc13
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2023-03-03 21:31:00 +0100

    [Feat] Use an internal version of Chrome (#95)
    * feat: update dependencies
    * feat: use local version of chrome
    * docs: add changelogs

--- modules/lighthouse/tests/api/Client.test.ts
+++ modules/lighthouse/tests/api/Client.test.ts
@@ -1,5 +1,4 @@
 import { jest } from "@jest/globals"
-import { LaunchedChrome } from "chrome-launcher"
 import { RunnerResult } from "lighthouse"
 import { Conf } from "../data/Conf.js"
 
@@ -9,20 +8,6 @@ jest.unstable_mockModule("lighthouse", () => ({
 const lighthouse = (await import("lighthouse")).default
 const mockedLighthouse = jest.mocked(lighthouse)
 
-jest.unstable_mockModule("node:child_process", () => ({
-  spawn: jest.fn(),
-}))
-const { spawn } = await import("node:child_process")
-
-jest.unstable_mockModule("chrome-launcher", () => ({
-  launch: jest.fn<() => Promise<LaunchedChrome>>().mockResolvedValue({
-    pid: 1111,
-    port: 1234,
-    process: spawn("ls"),
-    kill: () => Promise.resolve(),
-  }),
-}))
-await import("chrome-launcher")
 const { requestResult } = await import("../../src/api/Client.js")
 
 describe("Run an analysis", () => {
