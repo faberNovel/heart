8a2aaae1f334c4dd4cf49a7919a6abf5595c3810
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2023-03-03 21:13:04 +0100

    feat: use local version of chrome

--- modules/lighthouse/src/api/Client.ts
+++ modules/lighthouse/src/api/Client.ts
@@ -1,11 +1,15 @@
-import { launch } from "chrome-launcher"
-import lighthouse from "lighthouse"
 import type { LighthouseConfig, LighthouseResult } from "@fabernovel/heart-common"
+import lighthouse from "lighthouse"
+import puppeteer from "puppeteer"
 
+/**
+ *
+ * @link https://github.com/GoogleChrome/lighthouse/blob/main/docs/puppeteer.md#option-2-launch-chrome-with-lighthousechrome-launcher-and-handoff-to-puppeteer
+ */
 export async function requestResult(conf: LighthouseConfig): Promise<LighthouseResult> {
-  const chrome = await launch({
-    chromeFlags: [
-      "--headless",
+  const browser = await puppeteer.launch({
+    defaultViewport: null,
+    args: [
       "--no-sandbox",
       // The /dev/shm partition is too small in certain VM environments, causing Chrome to fail or crash (see http://crbug.com/715363).
       // Use this flag to work-around this issue (a temporary directory will always be used to create anonymous shared memory files).
@@ -13,15 +17,16 @@ export async function requestResult(conf: LighthouseConfig): Promise<LighthouseR
       "--disable-dev-shm-usage",
     ],
   })
+  const page = await browser.newPage()
 
-  const runnerResult = await lighthouse(conf.url, { port: chrome.port, output: "json" }, conf.config)
+  const runnerResult = await lighthouse(conf.url, { output: "json" }, conf.config, page)
   if (undefined === runnerResult) {
     return Promise.reject(
       "The analysis run, but Lighthouse did not return any result. Try to start your analysis again."
     )
   }
 
-  await chrome.kill()
+  await browser.close()
 
   return { ...runnerResult.lhr } // weird hacky thing
 }
