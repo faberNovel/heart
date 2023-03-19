04d0a3f2babaecdd893bd522a020b91da0a40c35
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2023-03-19 13:19:04 +0100

    feat: make the options value required and avoid the usage of both at the same time

--- modules/cli/src/command/BaseCommand.ts
+++ modules/cli/src/command/BaseCommand.ts
@@ -1,23 +1,40 @@
-import { Command } from "commander"
+import { Command, Option } from "commander"
+import { snakeCaseToCamelCase } from "../text/case.js"
 
 export type BaseOptions = Partial<{
   exceptListeners: string
   onlyListeners: string
 }>
 
+// the keys are used to create the options names:
+// - options long names: keys names
+// - options short names: first letter of the keys names
+const BASE_OPTIONS: { [key in keyof BaseOptions]-?: string } = {
+  exceptListeners: "except-listeners",
+  onlyListeners: "only-listeners",
+}
+
+function createExceptListenersOption(): Option {
+  return new Option(
+    `-le, --${BASE_OPTIONS.exceptListeners} <${snakeCaseToCamelCase(BASE_OPTIONS.exceptListeners)}>`,
+    "A comma-separated list of listener modules that will not be triggered after the analysis is done"
+  ).conflicts(snakeCaseToCamelCase(BASE_OPTIONS.onlyListeners))
+}
+
+function createOnlyListenersOption(): Option {
+  return new Option(
+    `-lo, --${BASE_OPTIONS.onlyListeners} <${snakeCaseToCamelCase(BASE_OPTIONS.onlyListeners)}>`,
+    "A comma-separated list of listener modules that will be triggered after the analysis is done"
+  ).conflicts(snakeCaseToCamelCase(BASE_OPTIONS.exceptListeners))
+}
+
 export function createBaseSubcommand(name: string, description: string): Command {
   const cmd = new Command(name)
 
-  cmd
-    .description(description)
-    .option(
-      "--except-listeners [exceptListeners]",
-      "A comma-separated list of listener modules that will not be triggered after the analysis is done"
-    )
-    .option(
-      "--only-listeners [onlyListeners]",
-      "A comma-separated list of listener modules that will be triggered after the analysis is done"
-    )
+  const exceptListenersOption = createExceptListenersOption()
+  const onlyListenersOption = createOnlyListenersOption()
+
+  cmd.description(description).addOption(exceptListenersOption).addOption(onlyListenersOption)
 
   return cmd
 }
