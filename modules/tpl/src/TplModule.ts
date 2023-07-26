9f0f4ee3635f83d0283b0d00b2081942f75525f3
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2023-07-26 21:44:56 +0200

    refactor: rename the "moduletpl" template module into the more concise "tpl" name

--- modules/tpl/src/TplModule.ts
+++ modules/tpl/src/TplModule.ts
@@ -1,6 +1,6 @@
 import { Module, logger, type ModuleMetadata } from "@fabernovel/heart-common"
 
-export class ModuleTplModule extends Module {
+export class TplModule extends Module {
   constructor(moduleMetadata: ModuleMetadata, verbose: boolean) {
     super(moduleMetadata, verbose)
 
--- modules/tpl/src/index.ts
+++ modules/tpl/src/index.ts
@@ -1,6 +1,6 @@
 import type { ModuleIndex, ModuleMetadata } from "@fabernovel/heart-common"
-import { ModuleTplModule } from "./ModuleTplModule.js"
+import { TplModule } from "./TplModule.js"
 
 export const initialize: ModuleIndex["initialize"] = (moduleMetadata: ModuleMetadata, verbose: boolean) => {
-  return new ModuleTplModule(moduleMetadata, verbose)
+  return new TplModule(moduleMetadata, verbose)
 }
