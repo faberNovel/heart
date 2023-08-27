1b70f51ecddbc88311e3cf2d1c6dd53eeaca3734
Bastien Gatellier <bastien.gatellier@fabernovel.com>
2023-08-27 17:18:48 +0200

    fix: error display

--- modules/cli/src/error/EnvironmentError.ts
+++ modules/cli/src/error/EnvironmentError.ts
@@ -3,8 +3,8 @@ import type { ErrorObject } from "ajv"
 export class EnvironmentError extends Error {
   constructor(errors: ErrorObject[]) {
     super(
-      "Something is wrong with your environment variables:\n" +
-        errors.map((error) => "- " + (error.message ?? ""))
+      "Something is wrong with your environment variables:" +
+        errors.map((error) => "\n- " + (error.message ?? ""))
     )
 
     // @see {@link https://github.com/Microsoft/TypeScript-wiki/blob/main/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work}
